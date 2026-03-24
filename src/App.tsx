4bb165b777c97ad7c1a35996b6e4ef2458094858
Nikodo <nicolasjoubert@icloud.com>
2026-03-24 16:52:53 +0100

    feat(layout): break out of 800px constraint for full-width hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import Header from "@/components/layout/Header.tsx";
-import { H1, Body } from "@/components/ui/Typography.tsx";
+import { Body } from "@/components/ui/Typography.tsx";
 import Button from "@/components/ui/ButtonPerso.tsx";
 import SectionLabel from "@/components/ui/SectionLabel.tsx";
 import Tag from "./components/ui/Tag.tsx";
 import Footer from "./components/layout/Footer.tsx";
 import { PROJECTS } from "@/lib/constants";
 import { ProjectCard } from "@/components/layout/ProjectCard.tsx";
+import HeroBanner from "./components/layout/HeroBanner.tsx";
 
 function App() {
   return (
     <>
       <Header />
-      <main className="max-w-[800px] mx-auto px-8 pt-32 pb-24 flex flex-col gap-6">
-        <H1 className="relative z-0">
-          Je construis des interfaces
-          <br />
-          qui durent.
-        </H1>
+      <main className="max-w-[1080px] mx-auto px-8 pt-18 pb-24 flex flex-col gap-6">
+        <HeroBanner />
         <Body>Frontend developer junior...</Body>
         <div>
-          <Button variant="primary">Voir mes projets</Button>
+          <Button variant="accent">Voir mes projets</Button>
         </div>
         <div>
           <Tag variant="default">React</Tag>
