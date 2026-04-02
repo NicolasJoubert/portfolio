acfbef8922125bad7881305335908666fe4e5143
Nikodo <nicolasjoubert@icloud.com>
2026-04-02 14:57:30 +0200

    fix: remove wrong import in ContactForm

--- src/components/sections/ContactForm.tsx
+++ src/components/sections/ContactForm.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { AnimatePresence } from "framer-motion";
 import { Controller, useForm } from "react-hook-form";
